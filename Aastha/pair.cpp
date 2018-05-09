7d20cfc0126e22684837e2d1e85630ea7afe8bc3
Aastha <38806427+aasthac67@users.noreply.github.com>
2018-05-10 00:02:55 +0530

    Update pair.cpp

---
+++ Aastha/pair.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool secondsort(const pair<int,int>&a,const pair<int,int>&b)
+     { return(a.second<b.second);}
+int main()
+{  pair<int,int>p[1000]=make_pair(0,0);
+int x,y,z,ch,q;
+cin>>q;
+while(q!=0)
+{cin>>ch;
+   switch(ch)
+   {case 1:cin>>x>>y>>z;
+          p[x].first=y;
+          p[x].second=z; 
+          break;
+    case 2:cin>>x;
+           if(x==1)
+           {sort(p,p+1000);}
+           else if(x==2)
+           {sort(p,p+1000,secondsort);}
+           break;
+    case 3:for(int i=0;i<1000;i++)
+           {cout<<"("<<p[i].first<<","<<p[i].second<<")";}
+           cout<<endl;
+           break;
+    default:break;
+   }
+   q--;
+}
+return 0;
+}
