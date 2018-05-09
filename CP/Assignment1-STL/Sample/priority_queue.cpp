9045818c4304c7a1cf3ab4552b57c46d3a09f31f
jelwin <jelwin13afc@gmail.com>
2018-05-09 23:39:25 +0530

    Assignment 1-JELWIN

--- CP/Assignment1-STL/Sample/priority_queue.cpp
+++ CP/Assignment1-STL/Sample/priority_queue.cpp
@@ -1,4 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-//Your code here
+int main()
+{
+priority_queue<pair<int,pair<int,int>>>p;
+int a,b,c,d,e;
+cin>>a;
+while(a--)
+{
+cin>>b;
+switch(b)
+{
+case 1:cin>>c>>d>>e;
+p.push(make_pair(c,make_pair(d,e)));
+break;
+case 2:
+cout<<"("<<p.top().first<<","<<p.top().second.first<<","<<p.top().second.second<<")";
+cout<<endl;
+break;
+case 3:p.pop();
+break;
+case 4:cout<<p.size()<<endl;
+break;
+}}}
