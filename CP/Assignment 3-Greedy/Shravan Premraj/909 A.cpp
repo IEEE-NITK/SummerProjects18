1c472855ce940976e1f64ff001b6cacbe7cc06ea
Katakuri1239 <shravanpremraj@gmail.com>
2018-05-23 06:33:50 +0530

    moidified

--- CP/Assignment 3-Greedy/Shravan Premraj/909 A.cpp
+++ CP/Assignment 3-Greedy/Shravan Premraj/909 A.cpp
@@ -1,28 +1,21 @@
-#include<iostream>
-#include<string.h>
 #include<bits/stdc++.h>
 #include<stdio.h>
 int main()
 {
 int k,j;
-    char a[30],b[10],c[10];
+    std::string a,b;
       std::cin>>a;
 
     std::cin>>b;
 
-    k=strlen(a);
-
-    std::cout<<a[0];
-
+    k=a.size();
+     std::cout<<a[0];
     for( j=1;j<k;j++)
-        {if(!(a[j]>b[0]))
+       
+           {if(a[j]<b[0])
            std::cout<<a[j];
         else
-        {
-            std::cout<<b[0];
-            exit(0);
-        }
-        }
-        if(j==k)
-        std::cout<<b[0];
+            break;
+           }
+         std::cout<<b[0];
 }
--- CP/Assignment 3-Greedy/Shravan Premraj/982 B.cpp
+++ CP/Assignment 3-Greedy/Shravan Premraj/982 B.cpp
@@ -1,42 +1,44 @@
-#include<iostream>
-#include<string.h>
+
+
 #include<bits/stdc++.h>
 #include<vector>
 int main()
 { int number,size,count=0,count1,count2=0;
- int c[20]; int e[20];
+ std::string c;
+ int g[1000000];
     std::cin>>number;
+    if(number==0)
+     exit(0);
     count1=number-1;
-    std::vector<int> d;
+    
     std::vector<std::pair <int,int>>f;
-    std::vector<std::pair<int,int>>::iterator it;
+   
     for(int loop=0;loop<number;loop++)
      {
           std::cin>>size;
           f.push_back(std::make_pair(size,loop+1));
 
      }
+       std::cin>>c;
       std::sort(f.begin(),f.end());
-      for(int loop=0;loop<number;loop++)
-      std::cout<<"\n"<<f[loop].first<<" "<<f[loop].second;
-      std::cout<<"\n\n";
-      for(int loop=0;loop<2*number;loop++)
+         for(int loop=0;loop<2*number;loop++)
       {
-          std::cin>>c[loop];
+          
 
-          if(c[loop-1]==0&&c[loop]==1&&(loop-1)>0)
-                   {std::cout<<f[count-1].second;
+          if(c[loop-1]=='0'&&c[loop]=='1'&&(loop-1)>=0)
+                   {std::cout<<f[count-1].second<<" ";
 
                     }
         else
-             {  if(c[loop]==0)
-                {std::cout<<f[count].second;
+             {  if(c[loop]=='0')
+                {std::cout<<f[count].second<<" ";
                   count++;
-                     if(c[loop-1]==0&&(loop-1)>=0)
+                     if(c[loop-1]=='0'&&(loop-1)>=0)
                      {
 
-                      d.push_back(f[count-2].second);
-                       e[count2]=count-2;
+                      g[count2]=f[count-2].second;
+                    
+                       
                       count2++;
 
                      }
@@ -44,11 +46,11 @@ int main()
 
                      else
                         {   if(count2==0)
-                             {std::cout<<f[count1].second;
+                             {std::cout<<f[count1].second<<" ";
                                count1--;
                                }
                             else
-                              {std::cout<<d[e[count2-1]];
+                              {std::cout<<g[count2-1]<<" ";
                               count2--;
 
                                }
