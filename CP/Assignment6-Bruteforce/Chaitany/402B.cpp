0149d73df4798ed8b8104dc880699d2b8cae880a
chaitany10 <chaitany.pandiya@gmail.com>
2018-06-05 02:28:52 +0530

    Assignment 6 -Chaitany

--- CP/Assignment6-Bruteforce/Chaitany/402B.cpp
+++ CP/Assignment6-Bruteforce/Chaitany/402B.cpp
@@ -5,22 +5,38 @@ int main()
 
     int n,k;
     cin>>n>>k;
-    int arr[n];int arr1[n];
+    int arr[n];pair <int,int> p[1001]={make_pair(10000,1000)};
     int time[1000];
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
-    for(int j=0;j<1000;j++)
+    for(int j=1;j<=1000;j++)
     {int ti=0;
-        for(int t=1;t<n;t++)
+
+        for(int t=0;t<n;t++)
         {
             if(arr[t]!=j+k*t)
                 ti+=1;
         }
-        time[j]=ti;
+        p[j].first=ti;
+        p[j].second=j;
 
     }
-    sort(time,time+1000);
-    cout<<time[0]<<endl;
+    sort(p,p+1001);
+    int x=p[0].second;
+    cout<<p[0].first<<endl;
+    if(arr[0]<x)
+            cout<<"+ "<<1<<" "<<(x)-arr[0]<<endl;
+        if(arr[0]>x)
+            cout<<"- "<<1<<" "<<-((x)-arr[0])<<endl;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]<x+k*i)
+        {
+            cout<<"+ "<<i+1<<" "<<(x+k*i)-arr[i]<<endl;
+        }
+        if(arr[i]>x+k*i)
+            cout<<"- "<<i+1<<" "<<-((x+k*i)-arr[i])<<endl;
+    }
 }
