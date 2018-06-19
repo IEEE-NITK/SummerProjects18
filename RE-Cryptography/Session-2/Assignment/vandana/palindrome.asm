57758e4380a64e52e5c61397b26d08e5ebee824f
valkyr13 <vandana.swj@gmail.com>
2018-06-19 19:10:03 +0530

    made changes to assignment-1

--- RE-Cryptography/Session-2/Assignment/vandana/palindrome.asm
+++ RE-Cryptography/Session-2/Assignment/vandana/palindrome.asm
@@ -1,11 +1,13 @@
+section.data
+
+str1: DB "Palindrome" , 0x0a ,0x00
+len1: equ $-str1
+str2: DB "Not Palindrome", 0x0a , 0x00
+len2: equ $-str2
+
 section .bss
 	
 str: resd 100		;Reserving 400 bytes for a str. This is an uninitialized variable.
-
-
-section .data
-
-new_line: db 0x0a, 0x00
 
 
 section .text
@@ -13,70 +15,58 @@ section .text
 
 _start:
 
-	lea rcx, [str]
-	mov rdx, 400
 	call stdin_read
 
-	mov r9, rax     
-                	
-	dec r9		; Now, r9 has the length of the string. 
-	mov r8, r9
-        mov r10,0x01
-	dec r9			.
-
-	call reverse_string
-	
-	lea rcx, [str]
-	inc r9
-	mov rdx, r9
-	call stdout_write
-
-	lea rcx, [new_line]
-	mov rdx, 0x02
-	call stdout_write
-
-	call exit_prog
-
-
-
-reverse_string:
-		
-        lea rdi, [str]		; rdi has the address of the first character.
-	
-	lea rsi, [str + r9]	; rsi has the address of the last character.
-
-
-_loop:
-	cmp r10,r8              ; comparision takes place same no. of types as the length of   
-                                  string 
-	je return
-
-	cmp byte[rdi], byte[rsi]
-        jne return
-	inc rdi
-	dec rsi
-
-	jmp _loop
-
-
-
-return:
-        ret
-
+length:
+	mov r9, rax         	
+	dec r9		 s;Now, r9 has the length of the string. 
+        mov r10,0x00     ;r10 has index 0 
+	dec r9		 ;r9 has the last index of the string.	
+        mov r8, r9       ;r8 has the last index of the string.
+        call reverse_string
 
 stdin_read:
         mov rax, 0x03
         mov rbx, 0x00
+        lea rcx, [str]
+	mov rdx, 400
         int 0x80
+        call length
 
-        ret
 
-stdout_write:
+reverse_string:
+		
+        lea rdi, [str]		; rdi has the address of the first character.	
+	lea rsi, [str + r9]	; rsi has the address of the last character.
+        
+
+_loop:
+	cmp r10,r8                                             
+	jge stdout_write
+	cmp byte[rdi], byte[rsi]
+        jne stdout_write
+	inc rdi
+	dec rsi
+        inc r10
+        dec r9
+	jmp _loop
+
+stdout_write_n:
         mov rax, 0x04
         mov rbx, 0x01
+        lea rcx [str2] 
+        mov rdx, len2
         int 0x80
+        call exit_prog
 
-        ret
+stdout_write:
+        
+        mov rax, 0x04
+        mov rbx, 0x01
+        lea rcx [str1] 
+        mov rdx, len1
+        int 0x80
+        call exit_prog
 
 exit_prog:
         mov rax, 0x01
