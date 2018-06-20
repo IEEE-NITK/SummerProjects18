5fdecc210694dfed34b9eb574d9eb666e7457324
Tanmai <vvijaytanmai@gmail.com>
2018-06-20 13:40:25 +0530

    files for crypto assignment 1 - phase 1, Tanmai

---
+++ RE-Cryptography/Sessions-5-&-6/Tanmai/tanmai-caesar_encryption.py
@@ -0,0 +1,20 @@
+#msg file
+file = open('tanmai-message-caesar.txt', 'r')
+msg = file.read()
+file.close()
+
+file = open('tanmai-caesar_ciphertext.txt', 'w')
+
+k = 13 #letter shift
+x = ''
+
+for ch in msg:
+	if ch.isalpha():
+		if ch.isupper():
+			x  += chr((ord(ch) + k - ord('A')) % 26 + ord('A'))
+		else:
+			x  += chr((ord(ch) + k - ord('a')) % 26 + ord('a'))
+	else:
+		x += ch
+file.write(x)
+file.close()
