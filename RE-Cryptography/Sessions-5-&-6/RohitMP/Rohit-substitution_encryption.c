2398b28186878bd20db64a4165b8af23186c5067
Rohit M P <rohitmp99@gmail.com>
2018-06-20 22:56:33 +0530

    made necessary changes

--- RE-Cryptography/Sessions-5-&-6/RohitMP/Rohit-substitution_encryption.c
+++ RE-Cryptography/Sessions-5-&-6/RohitMP/Rohit-substitution_encryption.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+char subs(char );
 void main()
 {
     char str[8];
@@ -7,12 +7,45 @@ void main()
     FILE* output = fopen("rohit-substitution_ciphertext.txt","w");
     fseek(input,0,2);
     int last = ftell(input);
-    printf("%d\n",last);
     fseek(input,0,0);
     while(ftell(input)<last-1)
     {
         char ch;
         fscanf(input,"%c",&ch);
-        fprintf(output,"%c",122-(ch-97));
+        ch=subs(ch);
+        fprintf(output,"%c",ch);
+    }
+}
+
+char subs(char ch)
+{
+    switch(ch)
+    {
+        case 'a':return 'z';
+        case 'b':return 'y';
+        case 'c':return 'x';
+        case 'd':return 'w';
+        case 'e':return 'g';
+        case 'f':return 'u';
+        case 'g':return 't';
+        case 'h':return 's';
+        case 'i':return 'f';
+        case 'j':return 'c';
+        case 'k':return 'p';
+        case 'l':return 'o';
+        case 'm':return 'n';
+        case 'n':return 'i';
+        case 'o':return 'l';
+        case 'p':return 'k';
+        case 'q':return 'm';
+        case 'r':return 'j';
+        case 's':return 'h';
+        case 't':return 'v';
+        case 'u':return 'r';
+        case 'v':return 'e';
+        case 'w':return 'd';
+        case 'x':return 'q';
+        case 'y':return 'b';
+        case 'z':return 'a';
     }
 }
