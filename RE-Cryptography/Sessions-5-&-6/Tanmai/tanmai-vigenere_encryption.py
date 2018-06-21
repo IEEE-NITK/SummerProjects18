fccef5db1c11420ab1ed92f23af38583ea4a882d
Tanmai <vvijaytanmai@gmail.com>
2018-06-21 15:40:32 +0530

    changes in crypto assignment - 1, made all lowercase

---
+++ RE-Cryptography/Sessions-5-&-6/Tanmai/tanmai-vigenere_encryption.py
@@ -0,0 +1,31 @@
+file = open('tanmai-keyfile-vigenere.txt', 'r')
+key = file.read()
+file.close()
+
+f2 = open('tanmai-vigenere_ciphertext.txt', 'w')
+
+i = 0
+with open('tanmai-message-vigenere.txt', 'r') as file:
+	while file:
+		ch = file.read(1)
+		if not ch:
+			break
+		if i == len(key):
+			i = 0
+		if ch.isalpha():
+			ch = ch.lower()
+			if ch.isupper():
+				ch = chr( (ord(ch)+ ord(key[i].upper()) - 2*ord('A')) % 26 + ord('A') )
+			else:
+				ch = chr( (ord(ch)+ ord(key[i].lower()) - 2*ord('a')) % 26 + ord('a') )
+		
+		i = i+1
+		f2.write(ch)
+		
+f2.close()
+file.close()
+
+
+
+
+
