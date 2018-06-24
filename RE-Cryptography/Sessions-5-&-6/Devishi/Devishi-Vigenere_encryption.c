e116af927662d31b9a8ef42bdee9057cfd73551c
DEvishi Suresh <deviparvathi18@yahoo.com>
2018-06-24 21:57:51 +0530

    Crypto-Assignment Phase 1-Devishi

--- RE-Cryptography/Sessions-5-&-6/Devishi/Devishi-Vigenere_encryption.c
+++ RE-Cryptography/Sessions-5-&-6/Devishi/Devishi-Vigenere_encryption.c
@@ -9,30 +9,11 @@ FILE *msg2=fopen("Devishi-message-vigenere.txt","r");
 FILE *ciph2=fopen("Devishi-vigenere-ciphertext.txt","w");
 FILE *key=fopen("Devishi-vigenere-keyfile.txt","r");
 
-if(!msg2)
-{
-printf("ERROR IN OPENING\n");
-exit(1);
-}
-
-else if(!ciph2)
-{
-printf("ERROR IN OPENING\n");
-exit(2);
-}
-
-else if(!key)
-{
-printf("ERROR IN OPENING\n");
-exit(3);
-}
-
-char e,c,d,a,keytxt[50];
-int i=0,j=0;
+char e,c,a,keytxt[6],message[14],cipher[14];
+int i=0,j=0,y=0,n,d;
 
 while((e = fgetc(key))!=EOF)
 {
-e=(e-'a'+1);
 keytxt[j]=e;
 j++;
 }
@@ -40,41 +21,36 @@ keytxt[j-1]='\0';
 
 while((c=fgetc(msg2))!=EOF)
 {
-if((isalpha(c))&&(keytxt[i]!='\0'))
-{
-if((c+keytxt[i])<='z')
-{
-d=c+keytxt[i];
-fputc(d,ciph2);
-}
-else if((c+keytxt[i])>'z')
-{
-d=(((c+keytxt[i])-'z')+'a'-1);
-fputc(d,ciph2);
-}
-}
-
-else if((isalpha(c))&&(keytxt[i]=='\0'))
-{
-i=0;
-if((c+keytxt[i])<='z')
-{
-d=c+keytxt[i];
-fputc(d,ciph2);
-}
-else if((c+keytxt[i])>'z')
-{
-d=(((c+keytxt[i])-'z')+'a'-1);
-fputc(d,ciph2);
-}
-}
-
-else
-{
-fputc(c,ciph2);
-}
+message[i]=c;
 i++;
 }
+message[i-1]='\0';
+//Cipher
+j=0;
+for(int k=0,n=strlen(keytxt);message[k]!='\0';k++,j++,y++)
+{
+if(j==n)
+{
+j=0;
+}
+if(isalpha(message[k]))
+{
+d=(message[k]-192+keytxt[j]);
+if(d>26)
+{
+d=d%26;
+}
+cipher[y]=d+96;
+
+}
+else
+{
+cipher[y]=message[k];
+j--;
+}
+}
+cipher[13]='\0';
+fprintf(ciph2,"%s",cipher);
 
 fclose(msg2);
 fclose(ciph2);
