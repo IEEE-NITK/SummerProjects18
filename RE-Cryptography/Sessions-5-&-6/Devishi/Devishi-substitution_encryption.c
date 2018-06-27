9e360f54c59297a11ab93ebb8c541939cb151a0c
DEvishi Suresh <deviparvathi18@yahoo.com>
2018-06-27 19:33:51 +0530

    Modified Substitution

--- RE-Cryptography/Sessions-5-&-6/Devishi/Devishi-substitution_encryption.c
+++ RE-Cryptography/Sessions-5-&-6/Devishi/Devishi-substitution_encryption.c
@@ -9,47 +9,79 @@ int main()
  FILE *msg3=fopen("Devishi-message-substitution.txt","r");
  FILE *ciph3=fopen("Devishi-substitution-ciphertext.txt","w");
  FILE *key3=fopen("Devishi-substitution-keytext.txt","r");
+
  if(!msg3)
  {
- printf("ERROR IN OPENING\n");
- exit(1);
+  printf("ERROR IN OPENING\n");
+  exit(1);
  }
 
  else if(!ciph3)
  {
- printf("ERROR IN OPENING\n");
- exit(2);
+  printf("ERROR IN OPENING\n");
+  exit(2);
  }
- char c,w,keyf[4],msg[9],p[9];
- int y=0,ciph[9],pciph[9];
+
+
+ char c,w,keyf[4],msg[9],ciph[9],pciph[9];
+
+ int y=0;
+
+//Storing the key in an array
+
  while((w=fgetc(key3))!=EOF)
  {
- keyf[y]=w;
- y++;
+
+  keyf[y]=w;
+  y++;
+
  }
+
  keyf[3]='\0';
+
  y=0;
- puts(keyf);
+
+ //Storing the message in an array
+
+
  while((c=fgetc(msg3))!=EOF)
  { 
- msg[y]=c;
- y++;
+   msg[y]=c;
+   y++;
  }
+
  msg[8]='\0';
- puts(msg);
+ 
  y=0;
+
+
  for(int i=0;i<7;i++,y++)
  {
- if(y==3)
- {y=0;}
- pciph[i]=(msg[i]-'A');
- ciph[i]= keyf[y]+pciph[i];
-}
+  
+   if(y==3)
+    {
+      y=0;
+    }
+ 
+ //The code for encrypting 
 
- for(int j=0;j<7;j++)
-{
- fprintf(ciph3,"%d 9",ciph[j]);
+   pciph[i]=(msg[i]-msg[i+1]+keyf[y]);
+ 
+   if(pciph[i]>'Z')
+   {
+     ciph[i]=((pciph[i]-'Z')+'A'-1);
+   }
+
+   else
+   {
+     ciph[i]=pciph[i];
+   }
+  
 }
+ ciph[7]=msg[7];
+ ciph[8]='\0';
+ fprintf(ciph3,"%s",ciph);
+
  fclose(msg3);
  fclose(ciph3);
  fclose(key3);
