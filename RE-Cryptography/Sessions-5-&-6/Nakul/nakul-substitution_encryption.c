e4620368f5d89200ee6311932f9ccf978f273830
Nakul Suresh <nakulsuresh98@gmail.com>
2018-06-30 13:33:43 +0400

    Modified substitution encryption

--- RE-Cryptography/Sessions-5-&-6/Nakul/nakul-substitution_encryption.c
+++ RE-Cryptography/Sessions-5-&-6/Nakul/nakul-substitution_encryption.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 void main(){
-char ct[1000],pt[1000];
+char ct[1000],pt[1000],k[26];
 FILE *f1;
 f1=fopen("nakul-substitution_message.txt","r");
 if(f1==NULL)
@@ -12,16 +12,24 @@ if(f1==NULL)
 fgets(pt,1000,f1);
 fclose(f1);
 
+FILE *fk;
+fk=fopen("nakul-substitution_Key.txt","r");
+if(fk==NULL)
+ {printf("can't access file\n");
+  exit(0);
+ }
+fgets(k,26,fk);
+fclose(fk);
+
 int i;
 for(i=0;pt[i]!='\0';i++)
-  {if(pt[i]>=97&&pt[i]<=122)
-     { pt[i]-=97;
-       ct[i]=(pt[i]+8)%26 +97;
-     }
-   else
-     ct[i]=pt[i];
-  }
-
+{ if(pt[i]>=97&&pt[i]<=122)
+   { int a =pt[i]-97;
+     ct[i]=k[a];
+   }
+  else
+   ct[i]=pt[i];
+}
 
 FILE *f2;
 f2=fopen("nakul-substitution_ciphertext.txt","w");
