186b3f7b2f975cd6b92f0a93fd4c8b3668f73048
Nakul Suresh <nakulsuresh98@gmail.com>
2018-07-01 16:58:02 +0400

    Indented code for easy readability

--- RE-Cryptography/Sessions-5-&-6/Nakul/nakul-substitution_encryption.c
+++ RE-Cryptography/Sessions-5-&-6/Nakul/nakul-substitution_encryption.c
@@ -4,39 +4,47 @@
 void main(){
 char ct[1000],pt[1000],k[26];
 FILE *f1;
-f1=fopen("nakul-substitution_message.txt","r");
-if(f1==NULL)
- {printf("can't access file\n");
-  exit(0);
- }
+f1 = fopen("nakul-substitution_message.txt","r");
+ if(f1==NULL)
+   {
+     printf("can't access file\n");
+     exit(0);
+   }
 fgets(pt,1000,f1);
 fclose(f1);
 
 FILE *fk;
-fk=fopen("nakul-substitution_Key.txt","r");
-if(fk==NULL)
- {printf("can't access file\n");
-  exit(0);
- }
+fk = fopen("nakul-substitution_Key.txt","r");
+ if(fk==NULL)
+   {
+     printf("can't access file\n");
+     exit(0);
+   }
 fgets(k,26,fk);
 fclose(fk);
 
+
 int i;
-for(i=0;pt[i]!='\0';i++)
-{ if(pt[i]>=97&&pt[i]<=122)
-   { int a =pt[i]-97;
-     ct[i]=k[a];
+for(i = 0; pt[i] != '\0'; i++)
+   { 
+     if(pt[i] >= 97 && pt[i] <= 122)
+       { 
+        int a = pt[i]-97;
+        ct[i] = k[a];
+       }
+     else
+        ct[i] = pt[i];
    }
-  else
-   ct[i]=pt[i];
-}
+
 
 FILE *f2;
-f2=fopen("nakul-substitution_ciphertext.txt","w");
-if(f2==NULL)
- {printf("can't access file\n");
-  exit(0);
- }
+f2 = fopen("nakul-substitution_ciphertext.txt","w");
+ if(f2==NULL)
+   {
+    printf("can't access file\n");
+    exit(0);
+   }
 fputs(ct,f2);
 fclose(f2);
+
 }
