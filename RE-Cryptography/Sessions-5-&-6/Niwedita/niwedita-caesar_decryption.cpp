fe22a36c7af13654637539452623e3242383f642
Niwedita17 <niwedita.dakshana2017@gmail.com>
2018-07-02 01:12:20 +0530

    crypto_assignment

---
+++ RE-Cryptography/Sessions-5-&-6/Niwedita/niwedita-caesar_decryption.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+	int key;
+	cin>>key;
+	ifstream fin;
+	fin.open("t1.txt",ios::in);
+	ofstream fout;
+	fout.open("niwedita-caesar_decrypted.txt",ios::out);
+	char my_char;
+	while(!fin.eof())
+	{
+		fin.get(my_char);
+		if(my_char>='a' && my_char<='z')
+		{
+			my_char = my_char - key;
+			if (my_char<'a')
+				my_char = my_char + 'z' -'a'+1;
+		}
+		else if (my_char>='A' && my_char<='Z')
+		{
+			my_char = my_char - key;
+			if (my_char<'A')
+			my_char = my_char + 'Z' -'A'+1;
+		}
+		fout << my_char;
+	}
+	fin.close();
+	fout.close();
+	return 0;
+}
